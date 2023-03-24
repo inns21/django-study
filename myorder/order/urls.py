6d5aeb8d8140906ab0f2496888d3d0a662d0dcb3
inns21 <inns2110@gmail.com>
2023-03-25 02:46:06 +0900

    first commit

---
+++ myorder/order/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+
+from .import views
+
+urlpatterns = [
+    path('',views.index),
+    # 주문접수
+    path('add_order/', views.add_order),
+    # 주문 확인
+    path('order_list/', views.order_list),
+    # 주문 수정
+    path('<int:id>/update/', views.update),
+    # 주문 삭제
+    path('<int:id>/delete/', views.delete),
+    # 주문 검색
+    path('order_list/search/', views.search),
+]
---
+++ myorder/order/views.py
@@ -0,0 +1,56 @@
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect 
+from .models import Order
+# Create your views here.
+def index(request):
+    return render(request, 'order/index.html')
+
+def add_order(request):
+    if request.method == 'GET':
+        return render(request,'order/order_form.html')
+    else :
+        order_text = request.POST['order_text']
+        price = request.POST['price']
+        address = request.POST['address']
+
+        Order.objects.create(
+            order_text = order_text,
+            price = price,
+            address = address
+        )
+        return HttpResponseRedirect('/order/')
+
+def order_list(request):
+    order_list = Order.objects.all()
+    return render(request, 'order/order_list.html',{'order_list':order_list})
+
+def delete(request, id):
+    Order.objects.get(id = id).delete()
+    return HttpResponseRedirect('/order/order_list/')
+
+def update(request,id):
+    order = Order.objects.get(id = id)
+    
+    if request.method == 'GET': 
+        return render(request, 'order/update_form.html', {'order':order})
+    else:
+        order.order_text = request.POST['order_text']
+        order.price = request.POST['price']
+        order.address = request.POST['address']
+        order.save()
+        return HttpResponseRedirect('/order/order_list/')
+
+def search(request):
+    get_text = request.GET['order_text']
+    select = request.GET['option']
+
+    order_list = []
+    if select == 'full':
+        order_list = Order.objects.filter(order_text = get_text)
+    elif select == 'part':
+        order_list = Order.objects.filter(order_text__contains = get_text)
+    elif select == 'front':
+        order_list = Order.objects.filter(order_text__startswith = get_text)
+
+    return render(request, 'order/order_list.html', {'order_list':order_list})
+        
